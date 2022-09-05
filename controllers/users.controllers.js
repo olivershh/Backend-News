4cfbd91d1d7137b88b31c65b5236de064de1ad11
olivershh <oliver.shackleton@gmail.com>
2022-09-05 18:06:00 +0100

    added .catch

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,7 +1,9 @@
 const { selectUsers } = require("../models/users.models");
 
 exports.getUsers = (req, res, next) => {
-  selectUsers().then((users) => {
-    res.status(200).send({ users: users });
-  });
+  selectUsers()
+    .then((users) => {
+      res.status(200).send({ users: users });
+    })
+    .catch(next);
 };
