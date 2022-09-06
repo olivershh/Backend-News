ed22dc215bfd799fe81672bd23e31541ccafe01d
olivershh <oliver.shackleton@gmail.com>
2022-09-06 12:00:29 +0100

    Tweaked error controllers

--- models/articles.model.js
+++ models/articles.model.js
@@ -5,8 +5,9 @@ exports.selectArticleById = (articleID) => {
     .query(`SELECT * FROM articles WHERE article_id = $1;`, [articleID])
     .then((queryData) => {
       const article = queryData.rows[0];
-      if (!article)
+      if (!article) {
         return Promise.reject({ status: 404, msg: "Article not found" });
+      }
       return article;
     });
 };
