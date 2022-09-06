5226c324be6b47484a44f4df745d549655c0bc51
olivershh <oliver.shackleton@gmail.com>
2022-09-06 10:39:18 +0100

    semicolon on query

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 
 exports.selectArticleById = (articleID) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleID])
+    .query(`SELECT * FROM articles WHERE article_id = $1;`, [articleID])
     .then((queryData) => {
       const article = queryData.rows[0];
       if (!article)
