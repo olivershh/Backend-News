7ce8eee28c662a3e0188858c79277e218d178854
olivershh <oliver.shackleton@gmail.com>
2022-09-08 11:16:09 +0100

    setup for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,22 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+const config = {};
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.ssl = { rejectUnauthorized: false };
 }
 
-module.exports = new Pool();
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL OR PGDATABASE not set");
+}
+
+// if (!process.env.PGDATABASE) {
+//   throw new Error('PGDATABASE not set');
+// }
+
+module.exports = new Pool(config);
--- listen.js
+++ listen.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
-const PORT = 9090;
+const { PORT = 9010 } = process.env; //need to update when soriting heroku
 
-app.listen(PORT, () => {
+app.listen(PORT, (err) => {
+  if (err) throw err;
   console.log(`Listening on port ${PORT}...`);
 });
